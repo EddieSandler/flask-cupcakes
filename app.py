75b27795aabf1a35f98f88720edb18e16d174bea
EddieSandler <ed.sandler@gmail.com>
2023-11-27 15:04:12 -0500

    part2 completed

--- app.py
+++ app.py
@@ -1,4 +1,8 @@
-"""Flask app for Cupcakes"""
+from flask import Flask, request, jsonify, render_template
+
+from models import db, connect_db, Cupcake
+
+
 app = Flask(__name__)
 app.app_context().push()
 app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql:///cupcakes'
@@ -6,5 +10,30 @@ app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.config['SQLALCHEMY_ECHO'] = True
 
 connect_db(app)
+
+#POST /api/cupcakes
+
+@app.route('/api/cupcakes')
+def list_cupcakes():
+    """Returns JSON w/ all cupcakes"""
+    all_cupcakes = [cupcake.serialize() for cupcake in Cupcake.query.all()]
+    return jsonify(cupcakes=all_cupcakes)
+
+@app.route('/api/cupcakes/<int:id>')
+def get_cupcake(id):
+    """view infor  on a cupcake"""
+    cupcake=Cupcake.query.get_or_404(id)
+    return jsonify(cupcake=cupcake.serialize())
+
+
+@app.route('/api/cupcakes',methods=['POST'])
+def make_cupcake():
+    """ Create a cupcake"""
+    new_cupcake = Cupcake(flavor=request.json["flavor"],size=request.json["size"],
+    rating=request.json["rating"],image=request.json["image"])
+    db.session.add(new_cupcake)
+    db.session.commit()
+    response_json = jsonify(cupcake=new_cupcake.serialize())
+    return (response_json, 201)
 
 
